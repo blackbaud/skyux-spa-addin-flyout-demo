c7e1edc19e97c99ef93f04b780f40dfdc7b25767
Blackbaud-ValKuppuswamy <Blackbaud-ValKuppuswamy@users.noreply.github.com>
2026-02-03 14:12:00 -0600

    Migrate to SKY UX 13 (#20)
    * Migrate to SKY UX 13
    
    * Updated addin client version

--- src/app/flyout-detail/index.component.ts
+++ src/app/flyout-detail/index.component.ts
@@ -1,10 +1,12 @@
 import {
   Component
 } from '@angular/core';
+import { FlyoutDetailComponent } from './flyout-detail.component';
 
 @Component({
     selector: 'app-flyout-detail-route-index',
     templateUrl: './index.component.html',
-    standalone: false
+    imports: [FlyoutDetailComponent],
+    standalone: true
 })
 export class FlyoutDetailRouteIndexComponent { }
