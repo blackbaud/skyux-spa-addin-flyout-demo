6360fad67bc70a99a54c370644569e0af64e7985
Val Kuppuswamy <Val.Kuppuswamy@blackbaud.me>
2026-06-12 12:59:03 -0500

    Migrated to SKY UX 14

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Fix for crossvent `global is not defined` error. The crossvent library is used by Dragula,
+// which in turn is used by multiple SKY UX components.
+// https://github.com/bevacqua/dragula/issues/602
+(window as any).global = window;
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
